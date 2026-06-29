174f6856689bd7045d13944de02371512a5e3dd6
CLAYMORE-DEV <claymore.gamedev@gmail.com>
2026-06-29 11:30:26 -0400

    Added web build script because I did a gamejam

---
+++ generate_builds_web.sh
@@ -0,0 +1,14 @@
+# Enter the bin directory
+cd bin/
+
+# Load the previously generated script encryption key
+export SCRIPT_AES256_ENCRYPTION_KEY=$(cat godot.gdkey)
+
+# Enter the godot directory
+cd godot/
+
+# Build linux export templates
+scons target=template_debug production=yes platform=web use_static_cpp=yes debug_symbols=no lto=auto
+scons target=template_release production=yes platform=web use_static_cpp=yes debug_symbols=no lto=auto
+scons target=template_debug production=yes platform=web use_static_cpp=yes debug_symbols=no lto=auto threads=no
+scons target=template_release production=yes platform=web use_static_cpp=yes debug_symbols=no lto=auto threads=no
