3390e902d3ea6543ea231ea1d1a929a2c5a283ee
CLAYMORE-DEV <claymore.gamedev@gmail.com>
2026-06-29 13:21:09 -0400

    Added 4.7 build scripts now that stable is out

---
+++ generate_builds_editor.sh
@@ -0,0 +1,15 @@
+# Enter the bin directory
+cd bin/
+
+# Load the previously generated script encryption key
+export SCRIPT_AES256_ENCRYPTION_KEY=$(cat godot.gdkey)
+
+# Enter the godot directory
+cd godot/
+
+# Build the editor
+scons target=editor production=yes platform=linuxbsd use_static_cpp=yes debug_symbols=no lto=auto linker=mold
+
+
+# Copy editor to godot4 executable
+sudo cp bin/godot.linuxbsd.editor.x86_64 /usr/bin/godot4
--- generate_builds_linux.sh
+++ generate_builds_linux.sh
@@ -7,9 +7,6 @@ export SCRIPT_AES256_ENCRYPTION_KEY=$(cat godot.gdkey)
 # Enter the godot directory
 cd godot/
 
-# Build the editor
-scons target=editor production=yes platform=linuxbsd use_static_cpp=yes debug_symbols=no lto=auto
-
 # Build linux export templates
 scons target=template_debug production=yes platform=linuxbsd use_static_cpp=yes debug_symbols=no lto=auto
 scons target=template_release production=yes platform=linuxbsd use_static_cpp=yes debug_symbols=no lto=auto
