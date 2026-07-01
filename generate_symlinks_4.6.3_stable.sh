54176bf7a1a47deeb221c855f7d47cfdcada25df
CLAYMORE-DEV <claymore.gamedev@gmail.com>
2026-07-01 06:58:22 -0400

    Updated 4.6 symlinks

--- generate_symlinks_4.6.3_stable.sh
+++ generate_symlinks_4.6.3_stable.sh
@@ -2,9 +2,6 @@ cd bin/godot/bin
 
 mkdir ~/.local/share/godot/export_templates/4.6.3.stable/
 
-# Copy editor to godot4 executable
-sudo cp godot.linuxbsd.editor.x86_64 /usr/bin/godot4
-
 # Link templates to the correct spot
 sudo ln -f godot.linuxbsd.template_debug.x86_64 ~/.local/share/godot/export_templates/4.6.3.stable/linux_debug.x86_64
 sudo ln -f godot.linuxbsd.template_release.x86_64 ~/.local/share/godot/export_templates/4.6.3.stable/linux_release.x86_64
